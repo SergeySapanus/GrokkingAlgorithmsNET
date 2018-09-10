928e595206130d75159caed9ce165fd8589b9958
Sergey Sapanus <s.sapanus@systtech.ru>
2018-09-10 15:16:00 +0300

    01_BinarySearch

---
+++ 01_BinarySearch/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_BinarySearch
+{
+    public class BinarySearch
+    {
+        public static int? IndexOf<T>(IReadOnlyList<T> sortedArray, T searchItem) where T : IComparable<T>
+        {
+            var low = 0;
+            var high = sortedArray.Count - 1;
+
+            while (low <= high)
+            {
+                var mid = (low + high) / 2;
+                var guess = sortedArray[mid];
+
+                var compareTo = guess.CompareTo(searchItem);
+
+                if (compareTo == 0)
+                    return mid;
+
+                if (compareTo > 0)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+
+            return null;
+        }
+    }
+}
--- GrokkingAlgorithmsNET/Program.cs
+++ GrokkingAlgorithmsNET/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using _01_BinarySearch;
 
 namespace GrokkingAlgorithmsNET
 {
@@ -6,7 +8,64 @@ namespace GrokkingAlgorithmsNET
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BinarySearchExample01();
+            BinarySearchExample02();
+            BinarySearchExample03();
+
+
         }
+
+        #region BinarySearch
+
+        static void BinarySearchExample01()
+        {
+            var array = new[] { 10, 11, 20, 24, 34, 39, 40, 44, 58, 90, 99 };
+            var item = 20;
+
+            Console.WriteLine($"Expected index: {array.ToList().IndexOf(item)}");
+
+            var index = BinarySearch.IndexOf(array, item);
+            var indexString = index.HasValue ? index.Value.ToString() : "null";
+
+            Console.WriteLine($"Actual index: {indexString}");
+        }
+
+        static void BinarySearchExample02()
+        {
+            var array = new string[11];
+
+            for (var i = 0; i < array.Length; i++)
+                array[i] = Guid.NewGuid().ToString();
+
+            array = array.OrderBy(a => a).ToArray();
+
+            var indexExpected = 10;
+
+            Console.WriteLine($"Expected index: {indexExpected}");
+
+            var index = BinarySearch.IndexOf(array, array[indexExpected]);
+            var indexString = index.HasValue ? index.Value.ToString() : "null";
+
+            Console.WriteLine($"Actual index: {indexString}");
+        }
+
+        static void BinarySearchExample03()
+        {
+            var array = new[]
+            {
+                Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()
+            }.OrderBy(a => a).ToArray();
+
+            var indexExpected = 0;
+
+            Console.WriteLine($"Expected index: {indexExpected}");
+
+            var index = BinarySearch.IndexOf(array, array[indexExpected]);
+            var indexString = index.HasValue ? index.Value.ToString() : "null";
+
+            Console.WriteLine($"Actual index: {indexString}");
+        }
+
+        #endregion BinarySearch
     }
 }
